9501bb70349fb6ec2718f15dd6bcc4aacca8219c
Denuwan Nammuni <denuwannammuni@gmail.com>
2025-01-07 10:20:47 +0530

    create input field

--- image_generator/src/Components/ImageGenerator/ImageGenerator.jsx
+++ image_generator/src/Components/ImageGenerator/ImageGenerator.jsx
@@ -9,9 +9,17 @@ const ImageGenerator = () => {
       <div className="header">Ai image <span>generator</span></div>
       <div className="img-loading">
         <div className="image"><img src={default_image} alt="" /></div>
+
+        </div>
+
+        <div className="search-box">
+          <input type="text" className='search-input' placeholder='Describe What You Want To See'/>
+          <div className="generate-btn">Generate</div>
+
+        </div>
       </div>
 
-    </div>
+    
   )
 }
 
