31d2d8b51893eed4215bfadd1abe60fb5862892c
Denuwan Nammuni <denuwannammuni@gmail.com>
2025-01-07 12:36:25 +0530

    finished

--- image_generator/src/Components/ImageGenerator/ImageGenerator.jsx
+++ image_generator/src/Components/ImageGenerator/ImageGenerator.jsx
@@ -7,11 +7,15 @@ const ImageGenerator = () => {
 
   const [image_url, setImage_url] = useState("/");
   let inputRef = useRef(null);
+  const [loading, setLoading] = useState(false);
+
 
   const imageGenerator = async () => {
     if (inputRef.current.value === "") {
       return 0
     }
+
+    setLoading(true);
     const imageGenerator = async () => {
       if (inputRef.current.value === "") {
         return;
@@ -35,7 +39,9 @@ const ImageGenerator = () => {
       );
 
       let data = await response.json();
-      console.log(data);
+      let data_array = data.data;
+      setImage_url(data_array[0].url)
+      setLoading(false);
     };
   }
 
@@ -44,6 +50,10 @@ const ImageGenerator = () => {
       <div className="header">Ai image <span>generator</span></div>
       <div className="img-loading">
         <div className="image"><img src={image_url === "/" ? default_image : image_url} alt="" /></div>
+        <div className="loading">
+          <div className={loading ? "loading-bar-full" : "loading-bar"}></div>
+          <div className={loading ? "loading-text" : "display-none"}>Loading...</div>
+        </div>
 
       </div>
 
